770d947bbd92cdd481f17cb2e2144e056b319c72
zenitzo <christian.santos0327@gmail.com>
2023-05-26 14:26:11 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,44 @@
+let count = 0;
+let thisCount = 0;
+
+const handlers = {
+  startInitFunctionOrder(data) {
+    count = data.count;
+  },
+
+  initFunctionInvoking(data) {
+    document.querySelector(".thingy").style.left = "0%";
+    document.querySelector(".thingy").style.width =
+      (data.idx / count) * 100 + "%";
+  },
+
+  startDataFileEntries(data) {
+    count = data.count;
+  },
+
+  performMapLoadFunction(data) {
+    ++thisCount;
+
+    document.querySelector(".thingy").style.left = "0%";
+    document.querySelector(".thingy").style.width =
+      (thisCount / count) * 100 + "%";
+  },
+};
+
+window.addEventListener("message", function (e) {
+  (handlers[e.data.eventName] || function () {})(e.data);
+});
+
+// TESTING
+
+function toggleMute() {
+  var audio = document.getElementById("audio");
+  var speakerIcons = document.getElementsByClassName("speakerIcon");
+
+  audio.muted = !audio.muted;
+
+  // Toggle the active class on the speaker icons
+  for (var i = 0; i < speakerIcons.length; i++) {
+    speakerIcons[i].classList.toggle("active");
+  }
+}
